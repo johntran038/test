764e76eba54ec33e02cff0e106c91e3796bfb35b
John Tran <johntran038@gmail.com>
2024-10-19 06:05:39 -0500

    test 3

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/test'>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
